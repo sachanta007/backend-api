024884e1094fec64d43beac975448495c703947d
Sai Rohith Achanta <sachanta@iu.edu>
2018-09-21 14:10:03 -0400

    Boiler plate is ready

--- Controllers/ex_controller.py
+++ Controllers/ex_controller.py
@@ -8,8 +8,7 @@ app.config.from_object(__name__)
 # app.config.from_object(__name__)
 @app.route("/")
 def check():
-	#return "Hello"
-	render_template('ex.html')
+	return "Hello"
 
 if __name__ == '__main__':
     #app.debug = True
