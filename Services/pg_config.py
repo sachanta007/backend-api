6081cf6c333c996a984767fdb40f60121c619934
Sai Rohith Achanta <sachanta@iu.edu>
2018-10-13 01:52:45 -0400

    SICECR2-26: Updated pg config
    SICECR2-26: Updated pg config

--- Services/pg_config.py
+++ Services/pg_config.py
@@ -4,7 +4,8 @@ class PgConfig:
 	@staticmethod
 	def db():
 		try:
-			conn = psycopg2.connect("dbname='brevwdnu' user='brevwdnu' host='elmer.db.elephantsql.com' password='fHFLQnWQe4e5OB3Bfx1oIxkPLtwl1Gev'")
+			conn = psycopg2.connect("dbname='d1osp41mpkgnb' user='eswrjkcmucapvr' host='ec2-174-129-221-240.compute-1.amazonaws.com' password='1063d5172fb023493e422a7a8ee24413b9da962aa8441d785a5e0bf5cf72d573'")
+			#psycopg2.connect("dbname='brevwdnu' user='brevwdnu' host='elmer.db.elephantsql.com' password='fHFLQnWQe4e5OB3Bfx1oIxkPLtwl1Gev'")
 			return conn
 		except:
 			return False
